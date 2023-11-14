3efb0081bebef8bfaf6a66264ae04aca761fb139
Albert <albert.yu.zh@gmail.com>
2023-11-13 17:08:07 -0800

    add open-graph add keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: '好室友™ 平台',
-  description: '湾区, 旧金山, 租房，房屋出租, 求租，合租，分租, 短期，长期',
+  description: '湾区,旧金山,租房,房屋出租,求租,合租,分租,短期,长期',
+  keywords: '湾区,旧金山,租房,房屋出租,求租,合租,分租,短期,长期',
+  openGraph: {
+    title: '好室友™ 平台',
+    description: '湾区,旧金山,租房,房屋出租,求租,合租,分租,短期,长期',
+  },
 }
 
 export default function RootLayout({
