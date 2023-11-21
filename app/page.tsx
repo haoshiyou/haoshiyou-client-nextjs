c30aa2589764a4c03459a74075bea82d957a1039
Albert <albert.yu.zh@gmail.com>
2023-11-20 18:00:42 -0800

    update home-detail metada

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import App from '@/pages/App';
 import { Metadata, ResolvingMetadata } from 'next';
+import { HAOSHIYOU_REQ_URL } from '@/constants';
  
-export async function generateMetadata(): Promise<Metadata> {
- 
-  return {
-    title: '好室友™平台',
-    viewport: { width: 390, userScalable: false },
-  }
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
 }
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+  ): Promise<Metadata> {
+  // read route params
+  const { id } = searchParams;
+  // fetch data
+  const product = { title: '' };
+  if (id) {
+    const res = await fetch(`${HAOSHIYOU_REQ_URL}/${id}`).then((res) => res.json());
+    product.title = res.title;
+  }
+  
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || []
+  const metadaTitle = product.title || '好室友™平台';
+  const metadaKeyword = product.title || '好室友™平台';
+  return {
+      title: metadaTitle,
+      viewport: { width: 390, userScalable: false },
+      keywords: metadaKeyword,
+      openGraph: {
+          title: metadaTitle,
+          description: metadaKeyword,
+          images: 'https://haoshiyou-client-nextjs.vercel.app/_next/static/media/haoshiyou_logo.5825016d.svg',
+          url: 'https://haoshiyou.org/',
+    },
+  }
+  }
 
 export default function Home() {
   return (
--- app/pages/HomeDetail/index.tsx
+++ app/pages/HomeDetail/index.tsx
@@ -1,44 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import HomeInfo from '@/components/HomeInfo';
-import { Metadata, ResolvingMetadata } from 'next';
-import { HAOSHIYOU_REQ_URL } from '@/constants';
 
 import styles from './index.module.css';
 
-type Props = {
-    params: { id: string }
-    searchParams: { [key: string]: string | string[] | undefined }
-  }
 
-export async function generateMetadata(
-{ params, searchParams }: Props,
-parent: ResolvingMetadata
-): Promise<Metadata> {
-// read route params
-const { id } = searchParams;
-// fetch data
-const product = { title: '' };
-if (id) {
-  const res = await fetch(`${HAOSHIYOU_REQ_URL}/${id}`).then((res) => res.json());
-  product.title = res.title;
-}
 
-// optionally access and extend (rather than replace) parent metadata
-// const previousImages = (await parent).openGraph?.images || []
-const metadaTitle = product.title || '好室友™平台';
-const metadaKeyword = product.title || '好室友™平台';
-return {
-    title: metadaTitle,
-    viewport: { width: 390, userScalable: false },
-    keywords: metadaKeyword,
-    openGraph: {
-    title: metadaTitle,
-    description: metadaKeyword,
-    images: 'https://haoshiyou-client-nextjs.vercel.app/_next/static/media/haoshiyou_logo.5825016d.svg',
-  },
-}
-}
 
 const HomePage: React.FC = () => {
     const searchParams = useSearchParams();
