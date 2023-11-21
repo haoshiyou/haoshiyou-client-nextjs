34b1a56d77f45703996368c0809d16236e6a83ea
Albert <albert.yu.zh@gmail.com>
2023-11-20 18:13:51 -0800

    update home-detail metada

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import App from '@/pages/App';
 import { Metadata, ResolvingMetadata } from 'next';
-import { HAOSHIYOU_REQ_URL } from '@/constants';
+import { HAOSHIYOU_REQ_URL, imgServicePrefix } from '@/constants';
+import _get from 'lodash/get';
+
+const logoImageUrl = 'https://haoshiyou-client-nextjs.vercel.app/_next/static/media/haoshiyou_logo.5825016d.svg';
  
 type Props = {
   params: { id: string }
@@ -15,16 +18,19 @@ export async function generateMetadata(
   // read route params
   const { id } = searchParams;
   // fetch data
-  const product = { title: '' };
+  const product = { title: '', imageIds: [] };
   if (id) {
     const res = await fetch(`${HAOSHIYOU_REQ_URL}/${id}`).then((res) => res.json());
     product.title = res.title;
+    product.imageIds = res.imageIds;
   }
   
   // optionally access and extend (rather than replace) parent metadata
   // const previousImages = (await parent).openGraph?.images || []
   const metadaTitle = product.title || '好室友™平台';
   const metadaKeyword = product.title || '好室友™平台';
+  const imageIds = _get(product, 'imageIds', []);
+  const metadataImageUrl = imageIds[0] ? `${imgServicePrefix}${imageIds[0]}.jpg` : logoImageUrl;
   return {
       title: metadaTitle,
       viewport: { width: 390, userScalable: false },
@@ -32,7 +38,7 @@ export async function generateMetadata(
       openGraph: {
           title: metadaTitle,
           description: metadaKeyword,
-          images: 'https://haoshiyou-client-nextjs.vercel.app/_next/static/media/haoshiyou_logo.5825016d.svg',
+          images: metadataImageUrl,
           url: 'https://haoshiyou.org/',
     },
   }
